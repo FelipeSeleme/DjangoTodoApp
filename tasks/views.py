d22eb4a4044c1ab04fba7431ac443e5de7207fc7
Felipe Seleme Ribeiro <felipeselemeribeiro@gmail.com>
2024-12-10 15:08:31 -0300

    updt

--- tasks/views.py
+++ tasks/views.py
@@ -17,5 +17,11 @@ def index(request):
             form.save()
         return redirect('/')
 
+
     context = {'tasks':tasks, 'form':form}
     return render(request, 'tasks/list.html', context)
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+
+    return render(request, 'tasks/update_task.html')
