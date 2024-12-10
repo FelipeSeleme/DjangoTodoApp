e245c1af9295b49b510f16e169e2a8be6f1e3d29
Felipe Seleme Ribeiro <felipeselemeribeiro@gmail.com>
2024-12-10 14:21:25 -0300

    updt

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import *
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the tasks index.")
+    tasks = Task.objects.all()
+
+    context = {
+        'tasks': tasks
+    }
+    return render(request, 'tasks/list.html', context)
