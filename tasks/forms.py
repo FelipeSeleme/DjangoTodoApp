f670159efbfba7fccbded218caaebdf7d811dadf
Felipe Seleme Ribeiro <felipeselemeribeiro@gmail.com>
2024-12-12 16:29:15 -0300

    updt

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,11 +1,10 @@
 from django import forms
 from django.forms import ModelForm
-
-from .models import *
-
+from .models import Task
 
 class TaskForm(forms.ModelForm):
-
     class Meta:
         model = Task
-        fields = '__all__'
+        fields = ['title', 'complete']  # Garantir que 'complete' esteja aqui
+
+    complete = forms.BooleanField(required=False, widget=forms.CheckboxInput)
--- tasks/views.py
+++ tasks/views.py
@@ -1,50 +1,45 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
 from .models import *
 from .forms import *
 
-# Create your views here.
-
-
+# Página inicial
 def index(request):
     tasks = Task.objects.all()
-
     form = TaskForm()
 
     if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
-        return redirect('/')
+        return redirect('/')  # Redireciona para a página principal
 
-
-    context = {'tasks':tasks, 'form':form}
+    context = {'tasks': tasks, 'form': form}
     return render(request, 'tasks/list.html', context)
 
 
+# Atualizar tarefa
 def updateTask(request, pk):
     task = Task.objects.get(id=pk)
-
     form = TaskForm(instance=task)
 
     if request.method == 'POST':
         form = TaskForm(request.POST, instance=task)
         if form.is_valid():
-            form.save()
-        return redirect('/')
+            form.save()  # Salva a tarefa atualizada
+        return redirect('/')  # Redireciona para a página principal
 
-    context = {'form':form}
-
+    context = {'form': form}
     return render(request, 'tasks/update_task.html', context)
 
 
+# Excluir tarefa
 def deleteTask(request, pk):
-    item = Task.objects.get(id=pk)
+    item = Task.objects.get(id=pk)  # Pega a tarefa a ser excluída
 
     if request.method == 'POST':
-        item.delete()
-        return redirect('/')
+        item.delete()  # Exclui a tarefa
+        return redirect('/')  # Redireciona para a página inicial
 
-    context = {'item':item}
+    context = {'item': item}
     return render(request, 'tasks/delete.html', context)
